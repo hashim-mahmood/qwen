bf65a51568836c48a352ee3b2c145dd7dc89c0f2
hashim-mahmood <hqureshivx@gmail.com>
2025-03-16 00:31:54 +0500

    added_paths

--- scripts/finetune_lora_vision.sh
+++ scripts/finetune_lora_vision.sh
@@ -12,6 +12,7 @@ GLOBAL_BATCH_SIZE=128
 BATCH_PER_DEVICE=4
 NUM_DEVICES=8
 GRAD_ACCUM_STEPS=$((GLOBAL_BATCH_SIZE / (BATCH_PER_DEVICE * NUM_DEVICES)))
+PER_DEVICE_EVAL_BATCH_SIZE=32
 
 # If you want to tune the `embed_token` with LoRA, You need to tune `lm_head` together
 # You should freeze the the merger also, becuase the merger is included in the vision_tower.
@@ -28,21 +29,22 @@ deepspeed src/training/train.py \
     --num_lora_modules -1 \
     --deepspeed scripts/zero3.json \
     --model_id $MODEL_NAME \
-    --data_path /path/to/your/training/data.json \
-    --image_folder /path/to/your/image/folder \
-    --eval_data_path /path/to/your/eval/data.json \
+    --data_path /workspace/training_data/final_data/train.json \
+    --image_folder /workspace/training_data/final_data/all_images \
+    --eval_data_path /workspace/training_data/final_data/val.json \
     --evaluation_strategy "steps" \
     --eval_steps 200 \
     --remove_unused_columns False \
     --freeze_vision_tower True \
-    --freeze_llm True \
-    --tune_merger False \
+    --freeze_llm False \
+    --tune_merger True \
     --bf16 True \
     --fp16 False \
     --disable_flash_attn2 False \
-    --output_dir output/lora_vision_test \
-    --num_train_epochs 1 \
+    --output_dir output/lora_slim_vlm_training_1 \
+    --num_train_epochs 10 \
     --per_device_train_batch_size $BATCH_PER_DEVICE \
+    --per_device_eval_batch_size $PER_DEVICE_EVAL_BATCH_SIZE \
     --gradient_accumulation_steps $GRAD_ACCUM_STEPS \
     --image_min_pixels $((256 * 28 * 28)) \
     --image_max_pixels $((1280 * 28 * 28)) \
@@ -57,5 +59,5 @@ deepspeed src/training/train.py \
     --lazy_preprocess True \
     --save_strategy "steps" \
     --save_steps 200 \
-    --save_total_limit 10 \
+    --save_total_limit 20 \
     --dataloader_num_workers 4
